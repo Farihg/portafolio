292539cded33e5f2060ecc62910ea69a199763c0
Agustin Farias Andres <402203@sistemas.frc.utn.edu.ar>
2026-03-29 01:25:25 -0300

    components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+// // @ts-check
+// import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+// // https://astro.build/config
+// export default defineConfig({});
+
+import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
+
+export default defineConfig({
+  output: 'server', // o 'static' según prefieras
+  adapter: vercel(),
+});
